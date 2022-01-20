875fb997c7fcb4a7796d9af95efd259a7d00e27b
Nicole <nicole.silfverling@gmail.com>
2022-01-20 20:59:12 +0100

    dependencies main, ForestService methods

--- sti-abstractions-service-impl/src/main/java/sti/abstractions/service/ForestServiceImpl.java
+++ sti-abstractions-service-impl/src/main/java/sti/abstractions/service/ForestServiceImpl.java
@@ -1,15 +1,26 @@
 package sti.abstractions.service;
 
 
+import sti.abstractions.domain.Owl;
+import sti.abstractions.domain.PineTree;
+import sti.abstractions.domain.Squirrel;
+
+import java.util.List;
+
 public class ForestServiceImpl implements ForestService {
 
-    public Owl createOwl(int wingSpan) {
-        Owl owl = new Owl(wingSpan);
-        return owl;
+    public Owl createOwl(int wingSpan, boolean hungry, int weight, String name) {
+        return new Owl(wingSpan, hungry, weight, name);
     }
 
     @Override
     public Squirrel createSquirrel(String name, boolean isHungry, double weight) {
         return null;
     }
+
+    @Override
+    public PineTree createPineTree(int numOfCones, List<Squirrel> squirrels, Owl owl, int age){return null;}
+
+
+
 }
--- sti-abstractions-service/src/main/java/sti/abstractions/service/ForestService.java
+++ sti-abstractions-service/src/main/java/sti/abstractions/service/ForestService.java
@@ -1,10 +1,22 @@
 package sti.abstractions.service;
 
+import sti.abstractions.domain.Owl;
+import sti.abstractions.domain.PineTree;
+import sti.abstractions.domain.Squirrel;
+
+import java.util.List;
+
 public interface ForestService {
 
-    public Owl createOwl(int wingSpan);
+    Owl createOwl(int wingSpan, boolean hungry, int weight, String name);
 
-    public Squirrel createSquirrel(String name, boolean isHungry,  double weight);
+    Squirrel createSquirrel(String name, boolean isHungry, double weight);
+
+    PineTree createPineTree(int numOfCones, List<Squirrel> squirrels, Owl owl, int age);
+
+
+
+
 
 
 }
