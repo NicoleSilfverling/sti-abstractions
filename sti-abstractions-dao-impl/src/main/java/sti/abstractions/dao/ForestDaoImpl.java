87e4e33274862ff34388b12bc6ad571ad767c53c
Nicole <nicole.silfverling@gmail.com>
2022-01-22 21:35:25 +0100

    qire ForestDao and impl, test in unitTest

--- sti-abstractions-dao-impl/src/main/java/sti/abstractions/dao/ForestDaoImpl.java
+++ sti-abstractions-dao-impl/src/main/java/sti/abstractions/dao/ForestDaoImpl.java
@@ -4,9 +4,22 @@ import sti.abstractions.domain.Owl;
 import sti.abstractions.domain.PineTree;
 import sti.abstractions.domain.Squirrel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class ForestDaoImpl implements ForestDao{
+
+    Connection connection = null;
+    Statement statement = null;
+
+    public Connection getConnection() throws SQLException{
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/stidb", "sti","sti");
+    }
+
+
     @Override
     public Owl createOwl(int wingSpan, boolean hungry, int weight, String name) {
         return null;
